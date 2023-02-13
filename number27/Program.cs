c5e1c9511bd0dcb7034ba2d75227606f7432d2ef
Ilya <l0stt.ttime@gmail.com>
2023-02-13 14:01:04 +0300

    1 com

---
+++ number27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Clear();
+int GetSum(int A)
+{
+    int count = A;
+    int sum = 0;
+    while (count > 0)
+    {
+        sum += count % 10;
+        count /= 10;
+    }
+    return sum;
+}
+
+Console.Write("Enter number A: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"сумму цифр в числе {N} равна {GetSum(N)}");
---
+++ number29/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8
+// элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+int[] GenArray(int range, int min, int max)
+{
+    int[] array = new int[range];
+    for (int i = 0; i < range; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] inarray)
+{
+    for (int i = 0; i < inarray.Length; i++)
+        Console.Write($"{inarray[i]}");
+}
+
+Console.Write("Enter number range: ");
+int rang = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number min: ");
+int mini = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number max: ");
+int maxi = int.Parse(Console.ReadLine()!);
+int [] array = GenArray(rang, mini, maxi);
+PrintArray(array);
+
+
+
+
+// Console.Clear();
+
+// int[] GetArray()
+// {
+//     int[] array = new int[8];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] inarray)
+// {
+//     for (int i = 0; i < inarray.Length; i++)
+//         Console.Write($"{inarray[i]} ");
+// }
+
+// int [] array = GetArray();
+// PrintArray(array);
